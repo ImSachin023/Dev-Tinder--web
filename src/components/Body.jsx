b7dde5eb83bbaa2692c83fd2c089ccba18c781d8
ImSachin023 <sachinabx3@gmail.com>
2026-01-24 17:42:27 +0530

    bug fix

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,5 +1,5 @@
 import { Outlet, useNavigate } from "react-router-dom";
-import Navbar from "./navbar";
+import Navbar from "./Navbar";
 import Footer from "./Footer";
 import { useDispatch, useSelector } from "react-redux";
 import axios from "axios";
